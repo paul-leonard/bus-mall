9cf54b5a587b8cc5d584ad468b3db5a85bf19219
paul-leonard <paul.ryan.leonard@gmail.com>
2020-07-21 16:37:56 -0700

    infinite loop occuring, TA Chance recommends some changes... this is a commit before changing it up

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@
 Assumptions:
 User session
 
-*** Feature List ***
-Pick out 3 random images to show.
-No repeat images within the three.
-Display images side-by-side-by-side.
-Once user clicks on an image, list three new.
-Track how many times a product was clicked on.
-Each user should get 25 clicks per session.
-After the maximum clicks/picks, turn off the event listener.
-Display a list of all products in the form "Product1 had x votes and was shown y times."
+*** Lab11 Feature List ***
+- Pick out 3 random images to show.
+- No repeat images within the three.
+- Display images side-by-side-by-side.
+- Once user clicks on an image, list three new.
+- Track how many times a product was clicked on.
+- Each user should get 25 clicks per session.
+- After the maximum clicks/picks, turn off the event listener.
+- Display a list of all products in the form "Product1 had x votes and was shown y times."
 
 *** Global Variables ***
 arrayOfProductObjects (array of ProductObjects)
@@ -42,14 +42,33 @@ Each Product will be its own object.
 *** Calculations within Methods ***
 math.random() will be used to determine which random products to show.  I will use the link to remember how to accurately receive an integer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
+
+*** Lab12 Feature List ***
+  - Each of the three products shown should not be repeated
+  - No product should be repeated from the previous screen
+  - Create a property that tracks how many times each product has been shown/increment the property each time product is shown
+  - Import ChartJS from CDN using a src link
+  - Use `<canvas>` tags to integrate chart in HTML
+  - display two bar charts:  one with times shown and one with times chosen
+  - locate bar charts below where the images were (maybe need to keep up the three photos?)
+  - charts should only appear after all voting is complete.
+  - Add an extra type of chart (suggest line chart on top of bar chart; bar=votes, line=shown)
+  - Missed 0.5 point last time, understand why and fix.
+  - extra: Also consider pie charts of times shown
+  - extra: what is the radar chart?
+  - extra: calculate chosen/shown rate.
+  - extra: sort results?
+  - extra: display top three
+  - extra: display top three with stats
 */
 
 // ************ Global Variables ***************
 var arrayOfProductObjects = [];
-var picksAllowedPerUserSession = 25; //per Lab11 will be 25
+var picksAllowedPerUserSession = 5; //per Lab11 will be 25
 var currentUserSessionClicks = 0;
 var productsToShowPerScreen = 3; //per Lab11 will be 3
 var randomPicks = [];
+var previousItemChoices = [];
 
 
 // *************** Function Definitions ***************
@@ -91,16 +110,49 @@ function pickItemChoices() {
     do {
       randomPicks[i] = Math.floor(Math.random() * ((arrayOfProductObjects.length-1) - 0 + 1)) + 0;
 
+      //below for loop checks current pick againist current randomPicks that are at indices less than the current number
       var redundantFlag = 0;
       for (var m = 0; m < i; m++) {
         if (randomPicks[i] === randomPicks[m]) {
           redundantFlag = 1;
-          // console.log('Found a REDUNDANT one!');
+          console.log('Found a REDUNDANT on same screen!');
           break;
+        } else {
+
+          console.log('previousItemChoices',previousItemChoices);
+          console.log('previousItemChoices.length',previousItemChoices.length);
+          if (previousItemChoices.length > 2) {
+            for (var n = 0; n < previousItemChoices.length; n++) {
+              if (randomPicks[i] === previousItemChoices[n]) {
+                redundantFlag = 1;
+                console.log('Found a REPEAT from previous screen!');
+                console.log('Repeat between screens check pass #', n);
+                break;
+              }
+            }
+          } else {
+            previousItemChoices.push(randomPicks[i]);
+          }
         }
       }
     } while (redundantFlag === 1);
+    // Thanka to Chance for his help trying to figure out this infinite loop and assigning values to the array and explaining execution order for JS
+    //a couple issues with the logic right now, but hopefully fixable
+    // on inital load, the previous array is going to get filled with randomPicks from first showing
+    //will need to reset to empty so that it can get filled again.
+    // rearrange the structure
+    //array setting to empty looks like 'array = [];'  decide on best place to say that
+    // after the big giant for loop, but before the function closes... try the clearing the array in a couple places
   }
+
+  console.log('previousItemChoices',previousItemChoices);
+  //previousItemChoices = randomPicks;  <-- This is a bad way to set an array value according to https://www.dyn-web.com/javascript/arrays/value-vs-reference.php
+  // for (var w = 0; w < randomPicks.length; w++) {
+  //   previousItemChoices[w] = randomPicks[w];
+  // }
+  //pass by reference; set a complete array equal to another array; 
+  console.log('previousItemChoices',previousItemChoices);
+  console.log('randomPicks',randomPicks);
 }
 
 
@@ -111,6 +163,8 @@ function displayItems() {
 
   //call function to determine which random products
   pickItemChoices();
+
+  // previousItemChoices = randomPicks; // <-- This is a bad way to set an array value
 
   //rely on methods to produce HTML to get each product on the page
   for (var i = 0; i < productsToShowPerScreen; i++) {
