97d59e3a29b68b000b040769d6d8f3ad62141236
paul-leonard <paul.ryan.leonard@gmail.com>
2020-07-22 22:57:17 -0700

    local storage set up for data to persist over new users refreshing the page

--- js/app.js
+++ js/app.js
@@ -43,7 +43,6 @@ Each Product will be its own object.
 math.random() will be used to determine which random products to show.  I will use the link to remember how to accurately receive an integer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 
-*** Lab12 Feature List ***
   - Each of the three products shown should not be repeated
   - No product should be repeated from the previous screen
   - Create a property that tracks how many times each product has been shown/increment the property each time product is shown
@@ -60,11 +59,21 @@ math.random() will be used to determine which random products to show.  I will u
   - extra: sort results?
   - extra: display top three
   - extra: display top three with stats
+
+*** Lab13 Feature List ***
+  - As a user, I would like my data to persistently track totals between page refreshes, so that I can keep track of the aggregate number of votes.
+      -Implement local storage into your current application
+      - Make sure the data persists across both browser refreshes and resets
+
+  *  Hints:
+    - Store the products array into local storage as a formatted JSON string
+    - Retrieve the products array from local storage and then utilize the JSON.Parse() function. Remember, you will have to send each item in the array back through constructor function.
+
 */
 
 // ************ Global Variables ***************
 ProductObject.arrayOfProductObjects = [];
-var picksAllowedPerUserSession = 25; // will be 25
+var picksAllowedPerUserSession = 5; // will be 25
 var currentUserSessionClicks = 0;
 var productsToShowPerScreen = 3; // will be 3
 var randomPicks = [];
@@ -73,13 +82,13 @@ var previousItemChoices = [];
 
 // *************** Function Definitions ***************
 
-function ProductObject (productName, src) {
+function ProductObject (productName, src, timesShownThisSession, timesChosenThisSession, shownAllTimeCount, chosenAllTimeCount) {
   this.productName = productName;
   this.src = src;
-  this.timesShownThisSession = 0;
-  this.timesChosenThisSession = 0;
-  this.ShownAllTimeCount = 0;
-  this.ChosenAllTimeCount = 0;
+  this.shownAllTimeCount = shownAllTimeCount;
+  this.chosenAllTimeCount = chosenAllTimeCount;
+  this.timesShownThisSession = timesShownThisSession;
+  this.timesChosenThisSession = timesChosenThisSession;
 
   ProductObject.arrayOfProductObjects.push(this);
 }
@@ -184,7 +193,24 @@ function reactToClick(event) {
         targetEl.appendChild(eachLiEl);
       }
 
+      //This renderGraphs displays counts for this session only
       renderGraphs();
+
+      // Do data modification to get All Time Data
+      for (var c = 0; c < ProductObject.arrayOfProductObjects.length; c++) {
+        ProductObject.arrayOfProductObjects[c].shownAllTimeCount = ProductObject.arrayOfProductObjects[c].shownAllTimeCount + ProductObject.arrayOfProductObjects[c].timesShownThisSession;
+        ProductObject.arrayOfProductObjects[c].chosenAllTimeCount = ProductObject.arrayOfProductObjects[c].chosenAllTimeCount + ProductObject.arrayOfProductObjects[c].timesChosenThisSession;
+        ProductObject.arrayOfProductObjects[c].timesShownThisSession = 0;
+        ProductObject.arrayOfProductObjects[c].timesChosenThisSession = 0;
+      }
+
+      // new function to plot all time data
+      renderAllTimeGraphs();
+
+      //save all of the vote/shown data to local storage
+      var marketSurveyDataLS = JSON.stringify(ProductObject.arrayOfProductObjects);
+      localStorage.setItem('marketSurveyDataLS',marketSurveyDataLS);
+      console.log(marketSurveyDataLS);
 
     } else {
       //display new set of products
@@ -463,31 +489,56 @@ function renderGraphs() {
 
 } //end of renderGraphs function
 
+function renderAllTimeGraphs() {
+  console.log('alltimegraphs');
+}
+
+
 
 // *************** Function Calls ***************
 
 var productGalleryTargetUl = document.getElementById('productGallery');
 productGalleryTargetUl.addEventListener('click', reactToClick);
 
-new ProductObject('R2D2 Rolling Suitcase','img/bag.jpg');
-new ProductObject('Banana Slicer','img/banana.jpg');
-new ProductObject('Tablet and TP Tower','img/bathroom.jpg');
-new ProductObject('Breathable Boots with no Fur','img/boots.jpg');
-new ProductObject('Toast, Egg, and Coffee Maker','img/breakfast.jpg');
-new ProductObject('Meatball Bubble Gum','img/bubblegum.jpg');
-new ProductObject('Inverted Chair','img/chair.jpg');
-new ProductObject('Cthulhu','img/cthulhu.jpg');
-new ProductObject('Beak for Dog','img/dog-duck.jpg');
-new ProductObject('Canned Dragon Meat','img/dragon.jpg');
-new ProductObject('Utensil Converter Pack for Pens','img/pen.jpg');
-new ProductObject('Sweeping Pet Booties','img/pet-sweep.jpg');
-new ProductObject('Pizza Scissors','img/scissors.jpg');
-new ProductObject('Shark Sleeping Bag','img/shark.jpg');
-new ProductObject('Tauntaun Sleeping Bag','img/tauntaun.jpg');
-new ProductObject('Canned Unicorn Meat','img/unicorn.jpg');
-new ProductObject('Ever-Fill Watering Can','img/water-can.jpg');
-new ProductObject('Never-Emptying Wine Glass','img/wine-glass.jpg');
-new ProductObject('Sweeping Baby Onesie','img/sweep.png');
-new ProductObject('Moving Octopus USB Drive','img/usb.gif');
+//get Market Survey Data from localStorage
+var marketSurveyDataFromLS = localStorage.getItem('marketSurveyDataLS');
+
+if(marketSurveyDataFromLS !== null){
+  var parsedMarketSurveyDataFromLS = JSON.parse(marketSurveyDataFromLS);
+
+  for (var c = 0; c < parsedMarketSurveyDataFromLS.length; c++) {
+
+    var productName = parsedMarketSurveyDataFromLS[c].productName;
+    var src = parsedMarketSurveyDataFromLS[c].src;
+    var timesShownThisSession = parsedMarketSurveyDataFromLS[c].timesShownThisSession;
+    var timesChosenThisSession = parsedMarketSurveyDataFromLS[c].timesChosenThisSession;
+    var shownAllTimeCount = parsedMarketSurveyDataFromLS[c].shownAllTimeCount;
+    var chosenAllTimeCount = parsedMarketSurveyDataFromLS[c].chosenAllTimeCount;
+
+    new ProductObject (productName, src, timesShownThisSession, timesChosenThisSession, shownAllTimeCount, chosenAllTimeCount);
+  }
+
+} else {
+  new ProductObject('R2D2 Rolling Suitcase','img/bag.jpg',0,0,0,0);
+  new ProductObject('Banana Slicer','img/banana.jpg',0,0,0,0);
+  new ProductObject('Tablet and TP Tower','img/bathroom.jpg',0,0,0,0);
+  new ProductObject('Breathable Boots with no Fur','img/boots.jpg',0,0,0,0);
+  new ProductObject('Toast, Egg, and Coffee Maker','img/breakfast.jpg',0,0,0,0);
+  new ProductObject('Meatball Bubble Gum','img/bubblegum.jpg',0,0,0,0);
+  // new ProductObject('Inverted Chair','img/chair.jpg',0,0,0,0);
+  // new ProductObject('Cthulhu','img/cthulhu.jpg',0,0,0,0);
+  // new ProductObject('Beak for Dog','img/dog-duck.jpg',0,0,0,0);
+  // new ProductObject('Canned Dragon Meat','img/dragon.jpg',0,0,0,0);
+  // new ProductObject('Utensil Converter Pack for Pens','img/pen.jpg',0,0,0,0);
+  // new ProductObject('Sweeping Pet Booties','img/pet-sweep.jpg',0,0,0,0);
+  // new ProductObject('Pizza Scissors','img/scissors.jpg',0,0,0,0);
+  // new ProductObject('Shark Sleeping Bag','img/shark.jpg',0,0,0,0);
+  // new ProductObject('Tauntaun Sleeping Bag','img/tauntaun.jpg',0,0,0,0);
+  // new ProductObject('Canned Unicorn Meat','img/unicorn.jpg',0,0,0,0);
+  // new ProductObject('Ever-Fill Watering Can','img/water-can.jpg',0,0,0,0);
+  // new ProductObject('Never-Emptying Wine Glass','img/wine-glass.jpg',0,0,0,0);
+  // new ProductObject('Sweeping Baby Onesie','img/sweep.png',0,0,0,0);
+  // new ProductObject('Moving Octopus USB Drive','img/usb.gif',0,0,0,0);
+}
 
 displayItems();
